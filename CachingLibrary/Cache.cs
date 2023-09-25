22401bf3ab8140fc6d475d579fc830f2746a5b89
Ian Kavanagh <ian.kavanagh@azyra.com>
2023-09-25 20:27:55 +0100

    Make public methods thread-safe with `ReadWriterLockSlim`

--- CachingLibrary/Cache.cs
+++ CachingLibrary/Cache.cs
@@ -8,6 +8,8 @@ public class LeastRecentlyUsedCache<TKey, TValue> where TKey : notnull
 
     private readonly LinkedList<CacheItem> _leastRecentlyUsed = new();
 
+    private readonly ReaderWriterLockSlim _lock = new();
+
     private int _capacity;
 
     public int Capacity
@@ -33,6 +35,11 @@ public class LeastRecentlyUsedCache<TKey, TValue> where TKey : notnull
         _cache = new Dictionary<TKey, LinkedListNode<CacheItem>>(capacity);
     }
 
+    ~LeastRecentlyUsedCache()
+    {
+        _lock.Dispose();
+    }
+
     /// <summary>
     /// Adds data to the cache. If the key already exists, the value is updated and the item is
     /// moved to the front of the list.
@@ -41,31 +48,39 @@ public class LeastRecentlyUsedCache<TKey, TValue> where TKey : notnull
     /// <param name="value">Data to add to catch</param>
     public void Add(TKey key, TValue value)
     {
-        if (_cache.TryGetValue(key, out LinkedListNode<CacheItem>? cacheNode))
+        _lock.EnterWriteLock();
+        try
         {
-            MoveNodeToFirst(cacheNode);
+            if (_cache.TryGetValue(key, out LinkedListNode<CacheItem>? cacheNode))
+            {
+                MoveNodeToFirst(cacheNode);
 
-            cacheNode.Value.Value = value;
+                cacheNode.Value.Value = value;
 
-            return;
+                return;
+            }
+
+            // key doesn't exist
+            if (_cache.Count >= Capacity)
+            {
+                CacheItem evictedItem = _leastRecentlyUsed.Last!.Value; // last is never null because capacity must be greater than 0
+
+                _cache.Remove(evictedItem.Key);
+                _leastRecentlyUsed.RemoveLast();
+
+                ItemEvicted?.Invoke(this, new CacheEventArgs(evictedItem.Key, evictedItem.Value));
+            }
+
+            CacheItem item = new(key, value);
+            LinkedListNode<CacheItem> newNode = new(item);
+
+            _cache.Add(key, newNode);
+            _leastRecentlyUsed.AddFirst(newNode);
         }
-
-        // key doesn't exist
-        if (_cache.Count >= Capacity)
+        finally
         {
-            CacheItem evictedItem = _leastRecentlyUsed.Last!.Value; // last is never null because capacity must be greater than 0
-
-            _cache.Remove(evictedItem.Key);
-            _leastRecentlyUsed.RemoveLast();
-
-            ItemEvicted?.Invoke(this, new CacheEventArgs(evictedItem.Key, evictedItem.Value));
+            _lock.ExitWriteLock();
         }
-
-        CacheItem item = new(key, value);
-        LinkedListNode<CacheItem> newNode = new(item);
-
-        _cache.Add(key, newNode);
-        _leastRecentlyUsed.AddFirst(newNode);
     }
 
     /// <summary>
@@ -75,14 +90,29 @@ public class LeastRecentlyUsedCache<TKey, TValue> where TKey : notnull
     /// <returns>The data corresponding to <paramref name="key" /></returns>
     public TValue? Get(TKey key)
     {
-        if (!_cache.TryGetValue(key, out LinkedListNode<CacheItem>? cacheNode))
+        _lock.EnterUpgradeableReadLock();
+        try
         {
-            return default;
+            if (!_cache.TryGetValue(key, out LinkedListNode<CacheItem>? cacheNode))
+            {
+                return default;
+            }
+
+            _lock.EnterWriteLock();
+            try
+            {
+                MoveNodeToFirst(cacheNode);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+            return cacheNode.Value.Value;
         }
-
-        MoveNodeToFirst(cacheNode);
-
-        return cacheNode.Value.Value;
+        finally
+        {
+            _lock.ExitUpgradeableReadLock();
+        }
     }
 
     private void MoveNodeToFirst(LinkedListNode<CacheItem> node)
